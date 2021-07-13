4c26caa0fec1414dec3903afd7e5da697652b03d
Denis Salem <denissalem@tuxfamily.org>
2021-07-13 12:48:16 +0200

    pass shellcheck

--- process_hunger.sh
+++ process_hunger.sh
@@ -17,16 +17,18 @@
 # You should have received a copy of the GNU General Public License
 # along with DServerSight. If not, see <http://www.gnu.org/licenses/>.
 
-# TODO : Watch memory as well.
+set -o errexit
+set -o nounset
+set -o pipefail
 
 for ARGUMENT in "$@"
 do
-    KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)   
 
     case "$KEY" in
-            cpu_threshold)       cpu_threshold=${VALUE} ;;     
-            mem_threshold)    mem_threshold=${VALUE} ;;     
+            cpu_threshold)	cpu_threshold=${VALUE} ;;     
+            mem_threshold)	mem_threshold=${VALUE} ;;     
             *)   
     esac    
 done
@@ -34,15 +36,17 @@ done
 if test -z "$cpu_threshold" || test -z "$mem_threshold"
 then
 	echo -e "usage: process_hunger.sh cpu_threshold=\"cpu load_value\"  mem_threshold=\"memory_usage_value\""
-	exit  -1
+	exit  255
 fi
 
-pid_cpu_mem_command=`ps aux | sort -nrk 3 | awk -v cpu_threshold=$cpu_threshold -v mem_threshold=$mem_threshold '$2 != "PID" && (int($3)>=cpu_threshold || int($4)>=mem_threshold) {out=$2"\t"$3"\t"$4"\t"$11; for (i=12;i<=NF;i++){out=out" "$i}; print "\t"out}';`
+pid_cpu_mem_command=$(ps aux | sort -nrk 3 | awk -v cpu_threshold="$cpu_threshold" -v mem_threshold="$mem_threshold" '$2 != "PID" && (int($3)>=cpu_threshold || int($4)>=mem_threshold) {out=$2"\t"$3"\t"$4"\t"$11; for (i=12;i<=NF;i++){out=out" "$i}; print "\t"out}';)
 if test ! -z "$pid_cpu_mem_command"
 then
 	mkdir -p /var/log/DServerSight/
 	touch /var/log/DServerSight/ProcessHunger.log
-	date +"%d-%m-%Y %H:%M:%S" >> /var/log/DServerSight/ProcessHunger.log 
-	echo -e "\tPID\tCPU\tMEM\tCOMMAND"  >> /var/log/DServerSight/ProcessHunger.log 
-	echo -e "$pid_cpu_mem_command"  >> /var/log/DServerSight/ProcessHunger.log 
+	{
+		date +"%d-%m-%Y %H:%M:%S"
+		echo -e "\tPID\tCPU\tMEM\tCOMMAND"
+		echo -e "$pid_cpu_mem_command"
+	}  >> /var/log/DServerSight/ProcessHunger.log 
 fi
